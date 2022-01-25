144b2016bb26ceb560cb20807b190add5c39dd99
Dhaval Pandey <68287498+dhavalpandey@users.noreply.github.com>
2022-01-25 08:59:54 +0000

    Update app.js

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ app.post("/options", async (req, res) => {
 
                 Subject.findOneAndUpdate(
                   { subject: key },
-                  { points: (points + data[key])/numberOfUpdates},
+                  { points: (points + data[key])/2},
                   (err, result) => {
                     if (err) {
                       console.log(err);
